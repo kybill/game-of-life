f06b35a1bb0c5189b8e00adcdb9178fb8bdf17c9
Jackson Parkinson <jacksonrp111@gmail.com>
2024-11-07 01:32:02 -0500

    Simulation code (buggy) and display code done

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::File, io::{BufReader, Read, Write}};
 
 pub struct Board {
-    chunks: HashMap<(i32, i32), Chunk>,
+    pub chunks: HashMap<(i32, i32), Chunk>,
 }
 
 impl Board {
@@ -11,22 +11,95 @@ impl Board {
         }
     }
 
+    pub fn load(&mut self, file: &str) {
+        let file_o = File::open(file).unwrap();
+        let mut buf_reader = BufReader::new(file_o);
+        let mut len_buf = [0u8; 8];
+        buf_reader.read_exact(&mut len_buf).unwrap();
+        let len = usize::from_be_bytes(len_buf);
+        self.chunks = HashMap::new();
+        
+        for _i in 0..len {
+            let mut cx_be = [0u8; 4];
+            let mut cy_be = [0u8; 4];
+            let mut data_be = [0u8; 8];
+            let mut lx_be = [0u8; 1];
+            let mut ux_be = [0u8; 1];
+            let mut ly_be = [0u8; 1];
+            let mut uy_be = [0u8; 1];
+
+            buf_reader.read_exact(&mut cx_be).unwrap();
+            buf_reader.read_exact(&mut cy_be).unwrap();
+            buf_reader.read_exact(&mut data_be).unwrap();
+            buf_reader.read_exact(&mut lx_be).unwrap();
+            buf_reader.read_exact(&mut ux_be).unwrap();
+            buf_reader.read_exact(&mut ly_be).unwrap();
+            buf_reader.read_exact(&mut uy_be).unwrap();
+
+            let cx = i32::from_be_bytes(cx_be);
+            let cy = i32::from_be_bytes(cy_be);
+            let data_u = u64::from_be_bytes(data_be);
+            let lx = u8::from_be_bytes(lx_be); 
+            let ux = u8::from_be_bytes(ux_be);
+            let ly = u8::from_be_bytes(ly_be);
+            let uy = u8::from_be_bytes(uy_be);
+
+            let mut chunk = Chunk::new();
+            chunk.data = data_u;
+            chunk.lowest_x = lx;
+            chunk.highest_x = ux;
+            chunk.lowest_y = ly;
+            chunk.highest_y = uy;
+            
+            self.chunks.insert((cx, cy), chunk);
+        }
+    }
+
+    pub fn save(&self, file: &str) {
+        // Format: No separators, just cx-cy-data for all chunks so i32-i32-u64
+        let mut file_o = File::create(file).unwrap();
+        let mut len_bytes = self.chunks.len().to_be_bytes();
+        file_o.write_all(&mut len_bytes).unwrap();
+
+        for (key, value) in &self.chunks {
+            let mut data = vec![];
+            data.extend_from_slice(&key.0.to_be_bytes());
+            data.extend_from_slice(&key.1.to_be_bytes());
+            data.extend_from_slice(&value.data.to_be_bytes());
+            data.extend_from_slice(&value.lowest_x.to_be_bytes());
+            data.extend_from_slice(&value.highest_x.to_be_bytes());
+            data.extend_from_slice(&value.lowest_y.to_be_bytes());
+            data.extend_from_slice(&value.highest_y.to_be_bytes());
+            file_o.write_all(&mut data).unwrap();
+        }
+    }
+
     pub fn clone(&self) -> Board {
         Board {
             chunks: self.chunks.clone()
         }
     }
 
+    pub fn chunk_count(&self) -> usize {
+        self.chunks.len()
+    }
+
     pub fn flip_cell(&mut self, x: i32, y: i32) {
         let (cx, cy, ix, iy) = self.get_chunk_coords(x, y);
         let chunk = self.get_or_create_chunk(cx, cy);
         chunk.flip_cell(ix, iy);
+        if chunk.data == 0 {
+            self.chunks.remove(&(cx, cy));
+        }
     }
 
     pub fn set_cell(&mut self, x: i32, y: i32, state: bool) {
         let (cx, cy, ix, iy) = self.get_chunk_coords(x, y);
         let chunk = self.get_or_create_chunk(cx, cy);
         chunk.set_cell(ix, iy, state);
+        if !state && chunk.data == 0 {
+            self.chunks.remove(&(cx, cy));
+        }
     }
 
     pub fn get_or_create_chunk(&mut self, chunk_x: i32, chunk_y: i32) -> &mut Chunk { 
@@ -67,21 +140,83 @@ impl Board {
 
     pub fn step_simulation(&mut self) {
         //let next_board = self.clone();
-        let array = self.generate_array();
+        let (array, to_check, x_offset, y_offset, width, height) = self.compress_chunk_array();
+        let mut true_cells = vec![];
+
+        // TODO: Apply game logic inside of modified_array using to_check coordinates
+        for (x, y) in to_check {
+            let val = array[y][x];
+            let mut neighbor_count = 0;
+
+            for i in 0..=2 {
+                for j in 0..=2 {
+                    if i == 1 && j == 1 {
+                        continue;
+                    }
+
+                    let nx = i32::try_from(x).unwrap() + i32::try_from(i).unwrap() - 1;
+                    let ny = i32::try_from(y).unwrap() + i32::try_from(j).unwrap() - 1;
+
+                    if nx < 0 || ny < 0 || nx > width - 1 || ny > height - 1 {
+                        continue;
+                    }
+
+                    if array[usize::try_from(ny).unwrap()][usize::try_from(nx).unwrap()] {
+                        neighbor_count += 1;
+                    }
+                }
+            }
+
+            /*if neighbor_count != 0 {
+                println!("{}", neighbor_count);
+            }*/
+
+            if val {
+                match neighbor_count {
+                    ..=1 => {},
+                    2..=3 => {
+                        true_cells.push((x, y));
+                    },
+                    4.. => {}
+                }
+            } else {
+                if neighbor_count == 3 {
+                    true_cells.push((x, y));
+                }
+            }
+        }
+
+        self.chunks = self.decompress_chunk_array(true_cells, x_offset, y_offset);
     }
 
-    pub fn generate_array(&self) -> (Vec<Vec<bool>>, i32, i32) {
+    pub fn decompress_chunk_array(&self, true_cells: Vec<(usize, usize)>, x_offset: i32, y_offset: i32) -> HashMap<(i32, i32), Chunk> {
+        let mut new_board = Board::new();
+
+        for (ax, ay) in true_cells {
+            let gx = i32::try_from(ax).unwrap() - x_offset;
+            let gy = i32::try_from(ay).unwrap() - y_offset;
+
+            new_board.set_cell(gx, gy, true);
+        }
+
+        return new_board.chunks;
+    }
+
+    pub fn compress_chunk_array(&self) -> (Vec<Vec<bool>>, Vec<(usize, usize)>, i32, i32, i32, i32) {
         // Generate a 2d array of bools for the current board state, padded by 1 around every edge
         // for quick calculations
         
         let (lx, ux, ly, uy) = self.get_bounds();
         let width = (ux - lx) + 3;
         let height = (uy - ly) + 3;
+        //println!("Bounds: X-({} - {}), Y-({} - {})", lx, ux, ly, uy);
 
         let conversion_x = -lx + 1;
         let conversion_y = -ly + 1;
 
         let mut array = vec![vec![false; width as usize]; height as usize];
+
+        let mut to_check = vec![];
 
         // Fill in the actual chunk data
         for (chunk_coords, chunk) in &self.chunks {
@@ -90,15 +225,30 @@ impl Board {
                     let (global_x, global_y) = self.get_global_coords(chunk_coords.0, chunk_coords.1, ix, iy);
                     
                     //println!("{} - {}, {} - {}", global_x, conversion_x, global_y, conversion_y);
-                    let ax: usize = (global_x + conversion_x).try_into().unwrap();
-                    let ay: usize = (global_y + conversion_y).try_into().unwrap();
+                    
+                    let ax_i = global_x + conversion_x;
+                    let ay_i = global_y + conversion_y;
+
+                    if ax_i < 0 || ay_i < 0 {
+                        continue;
+                    }
+
+                    let ax: usize = ax_i.try_into().unwrap();
+                    let ay: usize = ay_i.try_into().unwrap();
 
                     if ax >= width.try_into().unwrap() || ay >= height.try_into().unwrap() {
                         continue;
                     }
 
                     if chunk.get_cell(ix, iy) {
-                        println!("{} - {}", ix, iy);
+                        for i in 0..=2 {
+                            for j in 0..=2 {
+                                let coords = (ax + i - 1, ay + j - 1);
+                                if !to_check.contains(&coords) {
+                                    to_check.push(coords);
+                                }
+                            }
+                        }
                     }
 
                     array[ay][ax] = chunk.get_cell(ix, iy);
@@ -106,7 +256,7 @@ impl Board {
             }
         }
 
-        return (array, 0, 0);
+        return (array, to_check, conversion_x, conversion_y, width, height);
     }
 
     fn get_bounds(&self) -> (i32, i32, i32, i32) {
@@ -131,8 +281,14 @@ impl Board {
 
             let (cx, cy) = *key.unwrap();
             let chunk = val.unwrap();
-            let (lx, ly) = self.get_global_coords(cx, cy, chunk.lowest_x as i32, chunk.lowest_y as i32);
-            let (ux, uy) = self.get_global_coords(cx, cy, chunk.highest_x as i32, chunk.highest_y as i32);
+
+            let lowest_x = if cx >= 0 { chunk.lowest_x } else { chunk.highest_x } as i32;
+            let highest_x = if cx >= 0 { chunk.highest_x } else { chunk.lowest_x } as i32;
+            let lowest_y = if cy >= 0 { chunk.lowest_y } else { chunk.highest_y } as i32;
+            let highest_y = if cy >= 0 { chunk.highest_y } else { chunk.lowest_y } as i32;
+
+            let (lx, ly) = self.get_global_coords(cx, cy, lowest_x, lowest_y);
+            let (ux, uy) = self.get_global_coords(cx, cy, highest_x, highest_y);
 
             if lx < lower_x {
                 lower_x = lx;
@@ -158,10 +314,10 @@ impl Board {
 
 pub struct Chunk {
     data: u64,
-    lowest_x: u64,
-    highest_x: u64,
-    lowest_y: u64,
-    highest_y: u64
+    lowest_x: u8,
+    highest_x: u8,
+    lowest_y: u8,
+    highest_y: u8
 }
 
 impl Clone for Chunk {
@@ -203,14 +359,14 @@ impl Chunk {
         if val {
             // Set cell to true -> bitwise or
             self.data |= temp_bit;
-            self.update_bounds(x as u64, y as u64);
+            self.update_bounds(x as u8, y as u8);
         } else {
             // Set cell to false -> bitwise and with !temp_bit
             self.data &= !temp_bit;
         }
     }
 
-    fn update_bounds(&mut self, x: u64, y: u64) {
+    fn update_bounds(&mut self, x: u8, y: u8) {
         if x < self.lowest_x {
             self.lowest_x = x;
         } 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 pub mod board;
 
+use std::path::Path;
+
 use board::{Board, Chunk};
 use raylib::prelude::*;
 
@@ -10,42 +12,108 @@ fn main() {
         .resizable()
         .build();
 
-    rl.set_target_fps(60);
+    let args = std::env::args().collect::<Vec<String>>();
+    let static_board_file = if args.len() > 1 {
+        &args[1]
+    } else {
+        "./board.gol"
+    };
 
     /*
     Two boards stored: On-file static board (edit mode)
     Running board: Simulated from the static board (run mode)
     
     Static board is passed in from a binary file in command line arguments
+
+    If a non-existent file was passed, a blank static board is generated
+    and saved there on startup.
+
+    If no file argument is passed, a blank static board is created
+    at "./board.gol"
     */
 
-    let mut board = Board::new();
-    /*board.get_chunk_coords(0, 0);
-    board.get_chunk_coords(15, 15);
-    board.get_chunk_coords(16, 16);
-    board.get_chunk_coords(-1, -1);
-    board.get_chunk_coords(-13, -8);
-    board.get_chunk_coords(-1, -1);
-    board.get_chunk_coords(-16, -16);*/
+    let mut static_board = Board::new();
+    let mut running_board = Board::new();
 
-    board.set_cell(-1, -1, true);
-    board.set_cell(0, 0, true);
-    board.set_cell(1, 1, true);
-    board.set_cell(2, 2, true);
-
-    let chunk = board.get_chunk(0, 0).unwrap();
-    println!("{:0b}", chunk.get_raw());
-    println!("{} - {} - {}", chunk.get_cell(0, 0), chunk.get_cell(1, 1), chunk.get_cell(2, 2));
-
-    let (array, conv_x, conv_y) = board.generate_array();
-    for sub_array in array {
-        println!("{:?}", sub_array);
+    if Path::new(static_board_file).exists() {
+        static_board.load(static_board_file);
     }
+
+    // Mode is false when editing, true when running the simulation
+    let mut mode = false;
+    let cell_size = 20;
+    let padding = 1;
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
+        let (cx, cy) = (d.get_screen_width() / 2 - cell_size / 2, d.get_screen_height() / 2 - cell_size / 2);
 
-        d.clear_background(Color::WHITE);
+        d.clear_background(Color::BLACK);
+
+        let text = if !mode {
+            "Edit Mode"
+        } else {
+            "Run Mode"
+        };
+        d.draw_text(text, 0, 0, 20, Color::GREEN);
+        d.draw_text(&if mode { running_board.chunk_count() } else { static_board.chunk_count() }.to_string(), 0, 20, 20, Color::GREEN);
+
+        if d.is_key_pressed(KeyboardKey::KEY_SPACE) {
+            if !mode {
+                running_board = static_board.clone();
+            }
+
+            mode = !mode;
+        }
+
+        if mode {
+            if d.is_key_pressed(KeyboardKey::KEY_DOWN) {
+                running_board.step_simulation();
+            }
+
+            if d.is_key_down(KeyboardKey::KEY_RIGHT) {
+                running_board.step_simulation();
+            }
+        } else {
+            if d.is_key_down(KeyboardKey::KEY_LEFT_CONTROL) && d.is_key_pressed(KeyboardKey::KEY_S) {
+                static_board.save(static_board_file);
+                println!("Saved board");
+            }
+
+            if d.is_mouse_button_pressed(MouseButton::MOUSE_LEFT_BUTTON) {
+                let x = d.get_mouse_x() - cx;
+                let y = d.get_screen_height() - cy - d.get_mouse_y();
+
+                let mut bx = x / cell_size;
+                let mut by = y / cell_size;
+
+                if d.get_mouse_x() - cx < 0 {
+                    bx -= 1;
+                }
+                if d.get_mouse_y() - cy < 0 {
+                    by += 1;
+                }
+
+                static_board.flip_cell(bx, by);
+
+                println!("{} - {}", bx, by);
+            }
+        }
+
+        
+        let chunks = if mode { &running_board.chunks } else { &static_board.chunks };
+        for (key, val) in chunks {
+            for i in 0..8 {
+                for j in 0..8 {
+                    if !val.get_cell(i, j) {
+                        continue;
+                    }
+                    let (gx, gy) = running_board.get_global_coords(key.0, key.1, i, j);
+
+                    d.draw_rectangle(cx + gx * cell_size + padding, d.get_screen_height() - (cy + gy * cell_size + padding), cell_size - padding, cell_size - padding, Color::WHITE);
+                }
+            }
+        }
     }
 }
 
